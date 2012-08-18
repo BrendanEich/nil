569ddf4465ea37b65bfcbd295c0a69729b2ecb05
Brandon Benvie <brandon@bbenvie.com>
2012-08-18 12:32:57 -0400

    add support for building on platforms without prebuilt binaries

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var fs = require('fs'),
     path = require('path');
 
-if ((module === process.mainModule || !process.mainModule) && fs.existsSync('./build/Release/nil.node')) {
-  var nil = module.exports = require('./build/Release/nil.node').nil;
-} else {
-  var loc = path.resolve('./prebuilt', process.version.slice(0, 4), process.platform, process.arch, 'nil.node');
-  if (fs.existsSync(loc)) {
-    var nil = module.exports = require(loc).nil;
-  }
+var bindings = path.resolve('./build/Release/nil.node');
+if (fs.existsSync(bindings)) {
+  return module.exports = require(bindings).nil;
 }
 
+bindings = path.resolve('./prebuilt', process.version.slice(0, 4), process.platform, process.arch, 'nil.node');
+if (fs.existsSync(bindings)) {
+  return module.exports = require(bindings).nil;
+}
+
+
+if (process.env.npm_lifecycle_event === 'install') {
+  var PATH = process.env.PATH.split(process.platform === 'win32' ? ';' : ';'),
+      gyp = process.platform === 'win32' ? '/node-gyp.cmd' : '/node-gyp';
+
+  gyp = PATH.reduce(function(found, file){
+    return found || fs.existsSync(file = path.join(file, gyp)) ? file : null;
+  }, null);
+
+  if (gyp) {
+    require('child_process').spawn(gyp, ['rebuild'], {
+      cwd: __dirname,
+      env: process.env,
+      stdio: 'inherit'
+    });
+  }
+}
